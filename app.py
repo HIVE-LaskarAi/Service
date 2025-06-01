2dd7daf5ab6131ad54740aa4c03dd77f559be777
Xechi <msyuaib776@gmail.com>
2025-06-01 20:05:41 +0800

    Add swagger

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+from flasgger import Swagger
 from routes.riceRoute import rice_bp
 
 app = Flask(__name__)
+swagger = Swagger(app)
+
 app.register_blueprint(rice_bp, url_prefix='/api')
 
 @app.route('/')
 def home():
-    return "API Klasifikasi Hama Tanaman Padi aktif. Gunakan endpoint POST /api/predict"
+    return Response(
+        "API Klasifikasi Hama Tanaman Padi aktif.\n"
+        "Gunakan endpoint POST /api/predict untuk melakukan prediksi.\n"
+        "Lihat dokumentasi di /apidocs",
+        mimetype='text/plain'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
